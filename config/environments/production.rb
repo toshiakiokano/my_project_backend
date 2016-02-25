1f086adbbcf588c8e1a3b8ebb8018548557abde3
toshiaki okano <tokano@cnext.co.jp>
2016-02-25 15:13:14 +0900

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,14 +63,14 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'https://safe-waters-36187.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://safe-waters-36187' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     domain: 'gmail.com',
     address: 'smtp.gmail.com',
     port: 587,
     user_name: 'tokano@cnext.co.jp',
-    password: 'sldumwlhxgoumqbc',
+    password: 'toshi07aki25',
     authentication: :plain,
     enable_starttls_auto: true
   }
